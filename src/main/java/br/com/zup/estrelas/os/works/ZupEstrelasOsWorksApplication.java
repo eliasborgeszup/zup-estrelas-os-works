13855a2c7f82b16c3279fbb6b666396646cea95f
unknown <elias.borges@zup.com.br>
2020-10-22 13:15:40 -0300

    Criação de projeto spring OS Works

---
+++ src/main/java/br/com/zup/estrelas/os/works/ZupEstrelasOsWorksApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.estrelas.os.works;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZupEstrelasOsWorksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZupEstrelasOsWorksApplication.class, args);
+	}
+
+}
